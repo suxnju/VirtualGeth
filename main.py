512e1facd665b533c4b7a8093641c52b67bcff02
苏醒 <suxnju@smail.nju.edu.cn>
2021-03-08 20:52:41 +0800

    update data

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-class BaseComputation:
-    def __init__(self) -> None:
-        self._bytes = bytearray()
+def load_opcodes():
+    opcodes = []
+    with open("./data.txt","r",encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip().split(" ")
+            opcode = line[1]
+            if len(line) > 2:
+                args = opcode[-1]
+            else:
+                args = None
+            opcodes.append((opcode,args))
+    return opcodes
 
-    def extend_memory(self, start_position: int, size: int) -> None:
-        before_size = ceil32(len(self._memory))
-        after_size = ceil32(start_position + size)
+def load_init():
+    stack = [0,0,int('d579d4fe1e90a03d545e3d8c01dfc19c2ae3b26ad26ba994a1dec89a435a3dc0',16),0,int(0x2b8,16),0]
+    memory = []
+    storage = {}
 
-        before_cost = memory_gas_cost(before_size)
-        after_cost = memory_gas_cost(after_size)
+    return stack,memory,storage
 
-        if self.logger.show_debug2:
-            self.logger.debug2(
-                "MEMORY: size (%s -> %s) | cost (%s -> %s)",
-                before_size,
-                after_size,
-                before_cost,
-                after_cost,
-            )
+if __name__ == "__main__":
+    opcodes = load_opcodes()
+    stack,memory,storage = load_init()
 
-        if size:
-            if before_cost < after_cost:
-                gas_fee = after_cost - before_cost
-                self._gas_meter.consume_gas(
-                    gas_fee,
-                    reason=" ".join((
-                        "Expanding memory",
-                        str(before_size),
-                        "->",
-                        str(after_size),
-                    ))
-                )
-
-            self._memory.extend(start_position, size)
+    print()
