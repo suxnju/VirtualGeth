e8ecbc9fd3c8463c979b9138ff4b66ff9135a085
苏醒 <suxnju@smail.nju.edu.cn>
2021-03-10 21:19:30 +0800

    sync

--- Structure/utils.py
+++ Structure/utils.py
@@ -8,7 +8,11 @@ def keccak256(plain:str,is_hex:bool=False) -> str:
 
 
 if __name__ == "__main__":
-    to_hash = "00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000008"
-    print(keccak256(to_hash))
-    print(Web3.toHex(Web3.keccak(hexstr=to_hash)))
-    print(Web3.toHex(Web3.keccak(text=to_hash)))
+    to_hash = "000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000007"
+    # print(keccak256(to_hash))
+    # print(Web3.toHex(Web3.keccak(hexstr=to_hash)))
+    # print(Web3.toHex(Web3.keccak(text=to_hash)))
+
+    print(
+        Web3.toHex(Web3.keccak(hexstr="000000000000000000000000000000000000000000000000000000000000000a"+Web3.toHex(Web3.keccak(hexstr=to_hash)).lstrip("0x")))
+    )
